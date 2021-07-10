59a7eb1a836efb6cfd6bfba64f01d86012ef791c
budds300 <budds300@gmail.com>
2021-07-10 12:13:43 +0300

    import cloudinary in models and updated user table in image class

--- gram/models.py
+++ gram/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 import datetime
-from cloudinary import CloudinaryField
+from cloudinary.models import CloudinaryField
 # Create your models here.
 
 class Profile (models.Model):
@@ -18,10 +18,37 @@ class Image(models.Model):
     caption = models.CharField(max_length=60)
     pub_date = models.DateTimeField(blank=True,null=True,auto_now_add=True)
     likes = models.ManyToManyField(User, related_name='likes',blank=True)
-    user = models.ForeignKey(on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     
     class Meta:
         ''' class method to display images by published date'''
         ordering = ['pub_date']
     def __str__(self):
         return f'{self.image_name}'
+    
+class Comments(models.Model):
+    comment = models.CharField(max_length=300)
+    image = models.ForeignKey(Image,on_delete=models.CASCADE)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    created = models.DateField(null=True,auto_now_add=True)
+    
+    def __str__(self):
+        return self.comment
+    
+LIKE_CHOICES =(('Like','Like'),('Unlike','Unlike'))
+
+class Like(models.Model):
+    
+    user=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    image = models.ForeignKey(Image,on_delete=models.CASCADE, null=True)
+    value = models.CharField(max_length=10,null=True,default='Like',choices = LIKE_CHOICES)
+    
+    def __str__(self):
+        return self.image
+    
+class Follow(models.Model):
+    follower=models.ForeignKey(Profile,on_delete=models.CASCADE,related_name='following')
+    followed = models.ForeignKey(Profile,on_delete=models.CASCADE, related_name='followers')
+    
+    def __str__(self):
+        return f'{self.follower} Follow'
