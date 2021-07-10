2229a53e2da8a19a38bc82f88b25b43367a8a203
budds300 <budds300@gmail.com>
2021-07-10 19:42:01 +0300

    addition of search and post file

--- gram/urls.py
+++ gram/urls.py
@@ -2,6 +2,10 @@ from . import views
 from django.conf.urls import url
 from django.urls import path
 
+
 urlpatterns = [
-    url('', views.register, name = 'register')
+    path('', views.home, name='home'),
+    # path('post/<int:post_id>/', views.post, name='post'),
+    url(r'^post/(?P<image_id>\d+)', views.post, name='post'),
+    path('search/', views.search_results, name='search_results')
 ]
