2ef4d2dec8cf2d47321411a8b7ccab3e8c3d141e
budds300 <budds300@gmail.com>
2021-07-12 13:30:14 +0300

    addition of login and logout views

---
+++ instaclone/urls.py
@@ -0,0 +1,38 @@
+"""instaclone URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import url,include
+from django.contrib import admin
+from django.contrib.auth import views as auth_views,models
+from django.urls import path
+from instaapp import views as gram_views
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'',include('instaapp.urls')),
+    url(r'^login/',auth_views.LoginView.as_view(template_name='users/login.html'),name = 'login'),
+    url(r'^logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name = 'logout'),
+    path('login/',
+         auth_views.LoginView.as_view(template_name='users/login.html'),
+         name='login'),
+    path('logout/',
+         auth_views.LogoutView.as_view(template_name='users/logout.html'),
+         name='logout'),
+    path('profile/', gram_views.profile, name='profile'),
+    path('register/', gram_views.register, name='register'),
+    
+    
+]
